fc14c7b7fb50dd7f581b07d46cf268f15a5893aa
Karol Miksztal <fecx888@gmail.com>
2016-05-17 19:06:56 +0200

    naprawione action-cell (nie ma jej :D)

--- Prodzekt-war/src/java/ubezpieczalnia/controllers/LoginController.java
+++ Prodzekt-war/src/java/ubezpieczalnia/controllers/LoginController.java
@@ -7,6 +7,7 @@ package ubezpieczalnia.controllers;
 
 import ubezpieczalnia.utils.SessionManager;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,6 +17,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
 import ubezpieczalnia.entities.Klient;
 import ubezpieczalnia.entities.Konto;
 import ubezpieczalnia.entities.Pracownik;
@@ -30,25 +32,25 @@ import ubezpieczalnia.model.KontoEJB;
 @ManagedBean
 @SessionScoped
 public class LoginController implements Serializable, AbstractController<Konto> {
-    
+
     private Konto konto = new Konto();
     private List<Konto> kontaList = new ArrayList<>();
-    
+
     @EJB
     private KontoEJB kontoEJB;
-    
+
     public Konto getKonto() {
         return konto;
     }
-    
+
     public void setKonto(Konto konto) {
         this.konto = konto;
     }
-    
+
     public boolean checkLogged() {
         return SessionManager.getObjectFromSession("logged") != null;
     }
-    
+
     public String checkPermission() {
         String permissions = (String) SessionManager.getObjectFromSession("permission");
         if (permissions != null) {
@@ -56,12 +58,12 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         }
         return "";
     }
-    
+
     public Konto findKontoByLoginAndPassword() throws Exception {
         konto = kontoEJB.checkoutLogin(this.konto.getKontoLogin(), this.konto.getKontoHaslo());
         return konto;
     }
-    
+
     public String login() {
         try {
             if (konto != null && konto.getKontoLogin() != null && konto.getKontoHaslo() != null) {
@@ -78,28 +80,29 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         } catch (Exception ex) {
             Logger.getLogger("EXCEPTIONS").log(Level.WARNING, "Błędne dane logowania dla użytkownika=" + konto.getKontoLogin());
         }
-        
+
         SessionManager.addToSession("LOGIN_ERROR", "Błędny login lub hasło");
         return PageController.getPage("/login.xhtml");
     }
-    
+
     public String checkParam(String param) {
         Map<String, String> params = FacesContext.getCurrentInstance().
                 getExternalContext().getRequestParameterMap();
-        
+
         return params.get(param);
     }
-    
+
     public void logout() {
         if (SessionManager.getObjectFromSession("logged") != null) {
             SessionManager.destroySession();
         }
     }
-    
+
     public Klient getKlientAccount() {
         if (this.konto.getKlientCollection() == null) {
             return null;
         }
+        this.konto = kontoEJB.refresh(this.konto);
         for (Klient klient : this.konto.getKlientCollection()) {
             if (klient != null) {
                 System.out.println("IN NULL");
@@ -109,11 +112,12 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         }
         return null;
     }
-    
+
     public Pracownik getPracownikAccount() {
         if (this.konto.getPracownikCollection() == null) {
             return null;
         }
+        this.konto = kontoEJB.refresh(this.konto);
         for (Pracownik pracownik : this.konto.getPracownikCollection()) {
             if (pracownik != null) {
                 return pracownik;
@@ -121,9 +125,10 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         }
         return null;
     }
-    
+
     public ArrayList<Umowa> getUmowaPayments() {
         ArrayList<Umowa> temp = new ArrayList<>();
+        this.konto = kontoEJB.refresh(this.konto);
         for (Umowa umowa : this.getKlientAccount().getUmowaCollection()) {
             if (umowa != null && !umowa.getUmowaStatus().equals("ZAAKCEPTOWANA")) {
                 temp.add(umowa);
@@ -131,10 +136,10 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         }
         return temp;
     }
-    
+
     public ArrayList<Szkoda> getSzkodaPayments() {
         ArrayList<Szkoda> temp = new ArrayList<>();
-        
+        this.konto = kontoEJB.refresh(this.konto);
         for (Umowa umowa : this.getKlientAccount().getUmowaCollection()) {
             if (umowa != null) {
                 for (Szkoda szkoda : umowa.getSzkodaCollection()) {
@@ -146,30 +151,40 @@ public class LoginController implements Serializable, AbstractController<Konto> 
         }
         return temp;
     }
-    
+
+    public List<SelectItem> getUmowaSelectList() {
+        this.konto = kontoEJB.refresh(this.konto);
+        List<SelectItem> umowaSelectList = new ArrayList<>();
+        for (Umowa umowa : this.getKlientAccount().getUmowaCollection()) {
+            String label = umowa.getUmowaId() + " | " + umowa.getUmowaPojazdIdFk().getPojazdMarka() + " " + umowa.getUmowaPojazdIdFk().getPojazdModel();
+            umowaSelectList.add(new SelectItem(umowa.getUmowaId(), label));
+        }
+        return umowaSelectList;
+    }
+
     @Override
     public List<Konto> findAll() {
         kontaList = kontoEJB.findAll();
         return kontaList;
     }
-    
+
     @Override
     public Konto findById() throws Exception {
         konto = kontoEJB.findById(this.konto.getKontoId());
         return konto;
     }
-    
+
     @Override
     public String addNew() {
         this.konto = kontoEJB.addNew(this.konto);
         return PageController.getCurrentUrl();
     }
-    
+
     @Override
     public String update() {
         return PageController.getCurrentUrl();
     }
-    
+
     @Override
     public String delete() {
         kontoEJB.delete(this.konto);
